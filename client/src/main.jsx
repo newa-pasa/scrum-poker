827bd472de4a69c0ed9dbce8a73d67ea7faf906d
Dilip Shrestha <qdilip@gmail.com>
2025-05-26 06:36:15 +0545

    sesrver moved to subdirectory

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx' // Your main App component
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import './index.css'     // Your main CSS file (for global styles, if any)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
